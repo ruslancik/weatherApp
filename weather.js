df0504c763d9f40135329d6eb91cb4b36bbab733
ruslancik <44032621+ruslancik@users.noreply.github.com>
2019-06-23 16:13:32 +0300

    fixinf https

--- weather.js
+++ weather.js
@@ -6,7 +6,7 @@ class Weather {
 
     async getWeather(){
 
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}
         &appid=${this.apiKey}&units=metric`);
 
         const responseData = await response.json();
